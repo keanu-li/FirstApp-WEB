14a084f8908bf4a1fbe6bdc21373243cdf72d6a6
liguiqiang <1046566144@qq.com>
2016-11-09 14:32:17 +0800

    初始化工程。

---
+++ service/src/main/java/me/firstapp/service/App.java
@@ -0,0 +1,13 @@
+package me.firstapp.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
