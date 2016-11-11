fef6499fa114ca757aa34d9584e05bb39993ed2b
liguiqiang <1046566144@qq.com>
2016-11-11 16:14:08 +0800

    配置数据源。

--- api/src/main/java/config/WebConfig.java
+++ api/src/main/java/config/WebConfig.java
@@ -1,27 +1,24 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "me.firstapp.api" })
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/webapp/page/");
-		resolver.setSuffix(".jsp");
-		resolver.setExposeContextBeansAsAttributes(true);
-		return resolver;
-	}
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		resolver.setPrefix("/page/");
+//		resolver.setSuffix(".jsp");
+//		resolver.setExposeContextBeansAsAttributes(true);
+//		return resolver;
+//	}
 
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
--- api/src/main/java/me/firstapp/api/member/MemberApi.java
+++ api/src/main/java/me/firstapp/api/member/MemberApi.java
@@ -1,14 +1,26 @@
 package me.firstapp.api.member;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import me.firstapp.module.member.Member;
+import me.firstapp.service.member.MemberService;
+
 @Controller
 public class MemberApi {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String getMemberByName() {
-		return "home";
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping(value = "/getMemberByName", method = RequestMethod.GET)
+	public void getMemberByName(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println(111);
+		Member member = memberService.getMemberByName("keanu");
+		System.out.println(member.getMobile());
 	}
 }
